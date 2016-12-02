4644c3bfd670935ac28e59e037ee27d18ec8207d
wozniaklukasz <wozniak.lukasz.g@gmail.com>
2016-12-02 20:35:35 +0100

    Add countdown

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,15 @@ window.onload = function() {
 
 (jQuery)('.play-btn').on('click', function() {
     $('#game-over').css('display', 'none');
-    $('#intro').hide();
-    $('#game').show();
-    init();
+    $('header').hide('slow');
+    $('footer').hide('slow');
+    $('#intro').hide('slow');
+    $('#game').show('slow');
+    $('#steps').show('slow');
+
+    startGame(10, $('#step3'), false);
+
+
 });
 
 function init() {
@@ -17,5 +23,19 @@ function init() {
 
 function gameOver(player) {
     clearInterval(gameInterval);
+    $('header').show('slow');
+    $('footer').show('slow');
     $('#game-over').css('display', 'flex');
+}
+
+function startGame(duration, display, gameStarted) {
+    setInterval(function() {
+        if (duration > -1) {
+            display.text(duration--);
+        } else if (!gameStarted){
+            gameStarted = true;
+            $('#steps').hide('slow');
+            init();
+        };
+    }, 1000);
 }
